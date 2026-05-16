13ec2e273d5a8fecc51c64c90cee966228125f09
x5red78-HACKC1UB <x5red78@gmail.com>
2026-05-16 01:26:49 +0000

    finished

--- index.js
+++ index.js
@@ -2,3 +2,80 @@ let input;
 let input2;
 input=document.getElementById("input1");
 input2=document.getElementById("input2");
+enter=document.getElementById("enter");
+operation=document.getElementById("operation");
+result=document.getElementById("result");
+operationssign=operation.textContent;
+operation.addEventListener("click",() =>{
+if (operationssign==="+") {
+    operation.textContent = "-"
+    operationssign = operation.textContent;
+    console.log("operationssign:", operationssign, "operation:", operation);
+    console.log(integer1);
+    console.log(integer2);
+
+} else {
+    if (operationssign==="-") {
+        operation.textContent = "*"
+        operationssign = operation.textContent;
+        console.log("operationssign:", operationssign, "operation:", operation);
+ console.log(integer1);
+    console.log(integer2);
+    } else{
+        if(operationssign==="*"){
+operation.textContent = "/"
+operationssign = operation.textContent;
+console.log("operationssign:", operationssign, "operation:", operation);
+ console.log(integer1);
+    console.log(integer2);
+        }else{
+            if(operationssign==="/"){
+operation.textContent = "^"
+operationssign = operation.textContent;
+console.log("operationssign:", operationssign, "operation:", operation);
+ console.log(integer1);
+    console.log(integer2);
+        }else{
+            if(operationssign==="^"){
+operation.textContent="+"
+operationssign = operation.textContent;
+console.log("operationssign:", operationssign, "operation:", operation);
+ console.log(integer1);
+    console.log(integer2);
+            } 
+        }
+        }
+          
+    }
+}
+});
+enter.addEventListener("click",() =>{
+    let integer1=Number(input.value);
+let integer2=Number(input2.value);
+
+    if (operation.textContent==="+") {
+        result.textContent=integer1+integer2;
+    } else {
+        if (operation.textContent==="-") {
+            result.textContent=integer1-integer2;
+        } else {
+            if (operation.textContent==="*") {
+                result.textContent=integer1*integer2;
+            } else {
+                if (operation.textContent==="/") {
+                    result.textContent=integer1/integer2;
+                }else{
+                    if (operation.textContent==="^") {
+                    result.textContent=integer1**integer2;
+                }
+                    
+            }
+        }
+    }
+
+    }
+  if (isNaN(result.textContent)) {
+        window.alert("USE NUMBERS :(");
+        result.textContent= "Error";
+    }
+});
